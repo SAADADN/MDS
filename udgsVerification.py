8464295b47a786493f15c88015b8f9b41434c604
Saad <saadadn@gmail.com>
2022-04-12 11:30:43 +0300

    Update udgsVerification.py

--- udgsVerification.py
+++ udgsVerification.py
@@ -1,7 +1,12 @@
 import os
 import pickle
-from MDSconstraint import MDSconstraint
 import numpy as np
+
+def MDSconstraint(A,x):
+    for i in range(len(x)):
+        if np.dot(x,A[i])<1:
+            return False
+    return True
 
 for filename in os.listdir("udgs/dataSolution"):
    with open(os.path.join("udgs/dataSolution", filename), 'rb') as f:
