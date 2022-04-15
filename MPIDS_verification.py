c6d6ebe211e79a0bce83dc38bdd104323489f133
Saad <saadadn@gmail.com>
2022-04-15 07:42:26 +0300

    Add files via upload

---
+++ MPIDS_verification.py
@@ -0,0 +1,24 @@
+import numpy as np
+import os
+import pickle
+
+def MPIDSconstraint(A,x):
+    for i in range(len(x)):
+        if np.dot(x,A[i])<np.sum(A[i,:])/2:
+            return False
+    return True
+
+for filename in os.listdir("MPIDS_Table9"):
+   print(filename)
+   with open(os.path.join("MPIDS_Table9", filename), 'rb') as f:
+       network = pickle.load(f)
+   constr=lambda x:MPIDSconstraint(network['AdjacencyMatrix'].A, x)
+   if constr(network['Solution']):
+       print('Feasible')
+       print(np.sum(network['Solution']))
+   else:
+        print('Infeasible')
+
+
+
+
